006003091e85b41bef7e3e4c98df253156d59e40
Lize Chen <42664245+chenlize96@users.noreply.github.com>
2019-10-01 20:03:10 -0700

    bug found in semname

---
+++ phase1c/tests/semname.c
@@ -0,0 +1,105 @@
+#include <phase1.h>
+#include <phase1Int.h>
+#include <assert.h>
+#include <string.h>
+
+static int flag = 0;
+static int global = 3;
+
+static int
+Unblocks(void *arg)
+{
+    int sem = (int) arg;
+    int rc;
+
+    flag = 1;
+    USLOSS_Console("V on semaphore.\n");
+    rc = P1_V(sem);
+    assert(rc == P1_SUCCESS);
+    // will never get here as Blocks will run and call USLOSS_Halt.
+    return 12;
+}
+
+static int
+Blocks(void *arg) 
+{
+    int sem = (int) arg;
+    int rc;
+    int pid;
+
+    char name[15];
+    sprintf(name, "Unblocks%d", global);
+    rc = P1_Fork(name, Unblocks, (void *) sem, USLOSS_MIN_STACK, 5, 0, &pid);
+    assert(rc == P1_SUCCESS);
+    USLOSS_Console("P on semaphore, global %d.\n", global);
+    rc = P1_P(sem);
+    assert(rc == P1_SUCCESS);
+    assert(flag == 1);
+}
+
+static int
+Helper(void *arg) 
+{
+    int sem = (int) arg;
+    int rc;
+    int pid;
+    char name[15];
+    sprintf(name, "Blocks%d", global);
+    rc = P1_Fork(name, Blocks, (void *) sem, USLOSS_MIN_STACK, global, 0, &pid);
+    assert(rc == P1_SUCCESS);
+    global--;
+    sprintf(name, "Blocks%d", global);
+    rc = P1_Fork(name, Blocks, (void *) sem, USLOSS_MIN_STACK, global, 0, &pid);
+    assert(rc == P1_SUCCESS);
+    global--;
+    sprintf(name, "Blocks%d", global);
+    rc = P1_Fork(name, Blocks, (void *) sem, USLOSS_MIN_STACK, global, 0, &pid);
+    assert(rc == P1_SUCCESS);
+//********************************************************************************************************************************
+    char name1[P1_MAXNAME + 1] ;
+	char *namenull = '\0';                   // test null
+    rc = P1_SemName(sem, name1);
+	assert(rc == P1_SUCCESS);
+	
+	//rc = P1_SemName(sem,namenull);         // test null
+	//assert(rc != P1_NAME_IS_NULL);
+	
+	USLOSS_Console("%s\n", name1);
+    //assert(rc == P1_SUCCESS);
+    //assert(strcmp(name1, "sem") == 0);
+//********************************************************************************************************************************
+    rc = P1_SemFree(sem);
+    assert(rc == P1_SUCCESS);
+    USLOSS_Console("Test passed.\n");
+    USLOSS_Halt(0);
+    // should not return
+    assert(0);
+    return 0;
+}
+
+void
+startup(int argc, char **argv)
+{
+    int pid;
+    int rc;
+    int sem;
+
+    P1SemInit();
+//********************************************************************************************************************************
+// since name cannot exceed 80, so I made a name whose len is 80
+    rc = P1_SemCreate("abcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghik", 0, &sem);        
+//********************************************************************************************************************************
+    assert(rc == P1_SUCCESS);
+    // Blocks blocks then Unblocks unblocks it
+    rc = P1_Fork("Helper", Helper, (void *) sem, USLOSS_MIN_STACK, 6, 0, &pid);
+    assert(rc == P1_SUCCESS);
+    assert(0);
+}
+
+void dummy(int type, void *arg) {};
+
+void test_setup(int argc, char **argv) {}
+
+void test_cleanup(int argc, char **argv) {}
+
+void finish(int argc, char **argv) {}
