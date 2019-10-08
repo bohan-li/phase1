27823cbdac554dcd00fabc75d91f06d68f01f112
bohan li <li.3.141592653589@gmail.com>
2019-10-07 21:21:45 -0700

    finished, but has bugs

--- phase1d/phase1d.c
+++ phase1d/phase1d.c
@@ -21,6 +21,7 @@ static int sentinel(void *arg);
 typedef struct device {
 	int sid;
 	int abort;
+	int status;
 } Device;
 
 static Device devices[USLOSS_MAX_TYPES][USLOSS_MAX_UNITS];
@@ -49,7 +50,7 @@ startup(int argc, char **argv)
     USLOSS_IntVec[USLOSS_ILLEGAL_INT] = IllegalInstructionHandler;
 
     /* create the sentinel process */
-    int rc = P1_Fork("sentinel", sentinel, NULL, USLOSS_MIN_STACK, 6 , 0, &pid);
+    rc = P1_Fork("sentinel", sentinel, NULL, USLOSS_MIN_STACK, 6 , 0, &pid);
     assert(rc == P1_SUCCESS);
     // should not return
     assert(0);
@@ -65,33 +66,46 @@ P1_WaitDevice(int type, int unit, int *status)
 	if (!isValidUnit(type, unit)) return P1_INVALID_UNIT;
 	
     int     result = P1_SUCCESS;
+	
     // P device's semaphore
 	result = P1_P(devices[type][unit].sid);
 	assert(result == P1_SUCCESS);
 	
-    // set *status to device's status
-    return device[type][unit].abort ? P1_WAIT_ABORTED : P1_SUCCESS;
+	*status = devices[type][unit].status;
+    return devices[type][unit].abort ? P1_WAIT_ABORTED : P1_SUCCESS;
 }
 
 int 
 P1_WakeupDevice(int type, int unit, int status, int abort) 
 {
 	checkIfIsKernel();
+	if (!isValidType(type)) return P1_INVALID_TYPE;
+	if (!isValidUnit(type, unit)) return P1_INVALID_UNIT;
+	
     int     result = P1_SUCCESS;
+	
     // save device's status
-    // V device's semaphore 
+	devices[type][unit].status = status;
+	devices[type][unit].abort = abort;
+
+	result = P1_V(devices[type][unit].sid);
     return result;
 }
+static int tick = 0;
 
 static void
 DeviceHandler(int type, void *arg) 
 {
 	checkIfIsKernel();
-    // if clock device
-    //      P1_WakeupDevice every 5 ticks
-    //      P1Dispatch(TRUE) every 4 ticks
-    // else
-    //      P1_WakeupDevice
+	tick = (tick + 1) % 20;
+	int unit = (int) arg;
+	int status;
+	USLOSS_DeviceInput(type, unit, &status);
+    if (type == USLOSS_CLOCK_DEV) {
+		if (tick % 5 == 0) P1_WakeupDevice(type, unit, status, FALSE);
+        if (tick % 4 == 0) P1Dispatch(TRUE);
+	}
+    else P1_WakeupDevice(type, unit, status, FALSE);
 }
 
 static int
@@ -104,10 +118,22 @@ sentinel (void *notused)
     rc = P1_Fork("P2_Startup", P2_Startup, NULL, 4 * USLOSS_MIN_STACK, 2 , 0, &pid);
     assert(rc == P1_SUCCESS);
 
-    // enable interrupts
-    // while sentinel has children
-    //      get children that have quit via P1GetChildStatus
-    //      wait for an interrupt via USLOSS_WaitInt
+	P1EnableInterrupts();
+	int	tag, status;
+	P1_ProcInfo info;
+	while (1) {
+		rc = P1_GetProcInfo(P1_GetPid(), &info);
+		assert(rc == P1_SUCCESS);
+		if (info.numChildren == 0) break;
+		for (tag = 0; tag < 2; tag++) {
+			rc = P1GetChildStatus(tag, &pid, &status);
+			if (rc == P1_NO_QUIT) continue;
+			assert(rc == P1_SUCCESS);
+		}
+		
+		USLOSS_WaitInt();
+	}
+
     USLOSS_Console("Sentinel quitting.\n");
     return 0;
 } /* End of sentinel */
@@ -117,14 +143,23 @@ P1_Join(int tag, int *pid, int *status)
 {
 	checkIfIsKernel();
     int result = P1_SUCCESS;
-    // check for kernel mode
-    // disable interrupts
-    // do
-    //  get a child that has quit via P1GetChildStatus 
-    //  if no children have quit
-    //      set state to P1_STATE_JOINING vi P1SetState
-    //      P1Dispatch(FALSE)
-    // until either a child quit or there are no more children
+	
+	int interruptsWereEnabled = P1DisableInterrupts();
+	P1_ProcInfo info;
+	do {
+		result = P1GetChildStatus(tag, pid, status);
+		if (result == P1_INVALID_TAG || result == P1_NO_CHILDREN) return result;
+		else if (result == P1_NO_QUIT) {
+			P1SetState(P1_GetPid(), P1_STATE_JOINING, -1);
+			P1Dispatch(FALSE);
+		} else break;
+		
+		result = P1_GetProcInfo(P1_GetPid(), &info);
+		assert(result == P1_SUCCESS);
+		if (info.numChildren == 0) break;
+	} while (1);
+
+	if (interruptsWereEnabled) P1EnableInterrupts();
     return result;
 }
 
